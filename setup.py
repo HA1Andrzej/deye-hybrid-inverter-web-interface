37ca468f5e0f7e65822b45cbe482e016c4c351d8
HA1Andrzej <146762125+HA1Andrzej@users.noreply.github.com>
2025-03-13 22:23:17 +0100

    Update setup.py
    Poprawa adresu ip

--- setup.py
+++ setup.py
@@ -1,43 +1,56 @@
 import os
 import subprocess
+import times
+import socket
 
-def install_dependencies():
-    subprocess.run(['sudo', 'apt-get', 'update'], check=True)
-    subprocess.run(['sudo', 'apt-get', 'install', '-y', 'python3-pymodbus', 'hostapd', 'dnsmasq'], check=True)
+def installDependencies():
+   subprocess.run(['sudo', 'apt-get', 'update'])
+   subprocess.run(['sudo', 'apt-get', 'install', '-y', 'python3-pymodbus', 'hostapd', 'dnsmasq'])
 
-def create_service():
-    script_path = os.path.abspath(__file__)
-    backend_path = os.path.join(os.path.dirname(script_path), 'solarhub')
+def createService():
+   script_path = os.path.abspath(__file__)
+   backend_path = os.path.join(os.path.dirname(script_path), 'solarhub')
 
-    service_content = f"""
-[Unit]
-Description=SolarHub
-After=multi-user.target
+   service_content = f"""
+   [Unit]
+   Description=SolarHub
+   After=multi-user.target
 
-[Service]
-Type=simple
-ExecStart=/usr/bin/python3 {os.path.join(backend_path, "main.py")}
-WorkingDirectory={backend_path}
-Restart=always
-User=root
-Environment=HOME=/root
+   [Service]
+   Type=simple
+   ExecStart=/usr/bin/python3 {os.path.join(backend_path, "main.py")}
+   WorkingDirectory={backend_path}
+   Restart=always
+   User=root
+   Environment=HOME=/root
 
-[Install]
-WantedBy=multi-user.target
-    """
+   [Install]
+   WantedBy=multi-user.target
+   """
 
-    try:
-        with open("/etc/systemd/system/solarhub.service", 'w') as service_file:
-            service_file.write(service_content)
-        subprocess.run(["sudo", "systemctl", "daemon-reload"], check=True)
-        subprocess.run(["sudo", "systemctl", "enable", "solarhub.service"], check=True)
-        subprocess.run(["sudo", "systemctl", "start", "solarhub.service"], check=True)
-        print("Service solarhub.service successfully created and started.")
-    except Exception as e:
-        print(f"Error creating or starting the service: {e}")
+   try:
+      with open("/etc/systemd/system/solarhub.service", 'w') as service_file:
+         service_file.write(service_content)
+      subprocess.run(["sudo", "systemctl", "daemon-reload"], check=True)
+      subprocess.run(["sudo", "systemctl", "enable", "solarhub.service"], check=True)
+      subprocess.run(["sudo", "systemctl", "start", "solarhub.service"], check=True)
+      print(f"Service solarhub.service erfolgreich erstellt und gestartet.")
+   except Exception as e:
+      print(f"Fehler beim Erstellen oder Starten des Dienstes: {e}")
+
+
+def getIpAddress():
+   try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s.connect(("8.8.8.8", 80))
+      ip_address = s.getsockname()[0]
+      s.close()
+      return ip_address
+   except Exception as e:
+      return f"Fehler: {e}"
 
 if __name__ == "__main__":
-    install_dependencies()
-    create_service()
-    print("---------------------------------------")
-    print("Done, your service is up now. Visit the interface using the IP address of your device in a browser.")
+   installDependencies()
+   createService()
+   print("---------------------------------------")
+   print(f"Done, everything's ready to go. Visit the interface by entering the local IP-Adress (http://{getIpAddress()}) into a browser.")
